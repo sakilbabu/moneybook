5b606235e895acab91cbafdb61b547e0508755c3
Md Shakil Ahamed <md.shakilahamedxox72@gmail.com>
2021-08-26 07:49:23 -0400

    "updated project"

---
+++ lib/log_in.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:moneybook/signup.dart';
+import 'package:moneybook/task_page.dart';
+
+class login extends StatefulWidget {
+  const login({Key? key}) : super(key: key);
+
+  @override
+  _loginState createState() => _loginState();
+}
+
+class _loginState extends State<login> {
+  get passwordController => null;
+
+  get nameController => null;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("watch & earn"),
+        ),
+        body: Column(children: <Widget>[
+          Expanded(
+            child: ListView(
+              children: <Widget>[
+                Container(
+                    width: MediaQuery.of(context).size.width,
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      'watch & earn login',
+                      style: TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 30),
+                    )),
+                Container(
+                    width: MediaQuery.of(context).size.width,
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      'Sign in',
+                      style: TextStyle(fontSize: 20),
+                    )),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  padding: EdgeInsets.all(10),
+                  child: TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    controller: nameController,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Mail',
+                        prefixIcon: Icon(Icons.person)),
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                  child: TextField(
+                    keyboardType: TextInputType.visiblePassword,
+                    obscureText: true,
+                    controller: passwordController,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Password',
+                        prefixIcon: Icon(Icons.password)),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    //forgot password screen
+                  },
+                  child: Text('Forgot Password'),
+                ),
+                Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 50,
+                    padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    child: ElevatedButton(
+                      child: Text('Login'),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => taskpage()),
+                        );
+                      },
+                    )),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  child: Row(
+                    children: <Widget>[
+                      Text('Do not have account?'),
+                      TextButton(
+                        child: Text(
+                          'Sign up',
+                          style: TextStyle(fontSize: 10),
+                        ),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => signup()),
+                          );
+                        },
+                      ),
+                    ],
+                    mainAxisAlignment: MainAxisAlignment.center,
+                  ),
+                )
+              ],
+            ),
+          )
+        ]));
+  }
+}
